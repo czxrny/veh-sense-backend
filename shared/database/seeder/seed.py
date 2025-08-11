b304453edc99ef24022414b0844f67a403c9cc3e
czxrny <sczarnecki091@gmail.com>
2025-08-11 23:22:38 +0200

    fix: seeder only works when db tables are empty

--- shared/database/seeder/seed.py
+++ shared/database/seeder/seed.py
@@ -6,7 +6,7 @@ import bcrypt
 from insert_queries import userauth_insert_query, userinfo_insert_query, vehicle_insert_query, organization_insert_query
 from seed_data import users, organizations, vehicles
 
-load_dotenv("../../../.env") 
+load_dotenv() 
 
 conn = psycopg2.connect(
     host="localhost",
@@ -18,20 +18,33 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
-for user in users:
-    hashed_pw = bcrypt.hashpw(user["password"].encode(), bcrypt.gensalt()).decode()
-    cur.execute(userauth_insert_query, (user["email"], hashed_pw, user["role"]))
-    cur.execute(userinfo_insert_query, (user["user_name"], user["organization_id"], user["total_kilometers"]))
+cur.execute("SELECT * FROM user_auths;")
+rows = cur.fetchall()
+if len(rows) == 0:
+    for user in users:
+        hashed_pw = bcrypt.hashpw(user["password"].encode(), bcrypt.gensalt()).decode()
+        cur.execute(userauth_insert_query, (user["email"], hashed_pw, user["role"]))
+        cur.execute(userinfo_insert_query, (user["user_name"], user["organization_id"], user["total_kilometers"]))
+else:
+    print("Users table is not empty.")
 
-for organization in organizations:
-    cur.execute(organization_insert_query, organization)
+cur.execute("SELECT * FROM organizations;")
+rows = cur.fetchall()
+if len(rows) == 0:
+    for organization in organizations:
+        cur.execute(organization_insert_query, organization)
+else:
+    print("Organizations table is not empty.")
 
-for vehicle in vehicles:
-    cur.execute(vehicle_insert_query, vehicle)
+cur.execute("SELECT * FROM vehicles;")
+rows = cur.fetchall()  # i tutaj
+if len(rows) == 0:
+    for vehicle in vehicles:
+        cur.execute(vehicle_insert_query, vehicle)
+else:
+    print("Vehicles table is not empty.")
 
 conn.commit()
 
 cur.close()
 conn.close()
-
-print("Sucessfully seeded the database.")
