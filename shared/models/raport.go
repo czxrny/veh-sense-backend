b5fd5f86d9eb5d457d0cd76d9101ee646078125a
czxrny <sczarnecki091@gmail.com>
2025-12-29 20:48:34 +0100

    refactor: updated raport struct with new fields

--- shared/models/raport.go
+++ shared/models/raport.go
@@ -15,13 +15,11 @@ type Raport struct {
 	VehicleID           int       `json:"vehicle_id"`
 	FrameTime           time.Time `json:"frame_time"`
 	StopTime            time.Time `json:"stop_time"`
-	AvgFuel             float64   `json:"avg_fuel"`
-	MaxFuel             float64   `json:"max_fuel"`
-	ConsumedFuel        float64   `json:"consumed_fuel"`
-	KilometersTravelled float64   `json:"kilometers_travelled"`
 	AccelerationStyle   string    `json:"acceleration_style"`
 	BrakingStyle        string    `json:"braking_style"`
 	AverageSpeed        float64   `json:"average_speed"`
+	MaxSpeed            float64   `json:"max_speed"`
+	KilometersTravelled float64   `json:"kilometers_travelled"`
 }
 
 type RaportFilter struct {
